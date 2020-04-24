72eb7d752ca98fdd0ff4f1431e194a27e2cdcfd3
@Maiggk <miguelangelalvarez3@gmail.com>
2020-04-24 10:56:57 -0500

    Cambio de flujo en tabla authors a users(generada por el sistema)

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.models import *
+from .models import p_post,CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+#Admins site titles
+admin.site.site_header = "Plantika Administracion";
+admin.site.site_title = "Plantika";
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(CustomUser, UserAdmin)
+
+#Exclude areas here
+admin.site.unregister(Group)
+
+@admin.register(p_post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['name', 'sub_name', 'post', 'author', 'cover_image','schedule_post']
+    pass
--- Blog/models.py
+++ Blog/models.py
@@ -1,43 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
+from django.conf import settings
 
-# Create your models here.
-@python_2_unicode_compatible
-class p_author(models.Model):
-    alias = models.CharField(max_length=45)
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45,null=True)
-    about = models.CharField(max_length=300,null=True)
-    profile_img = models.CharField(max_length=45,null=True)
-    created = models.CharField(max_length=45)
-    password = models.CharField(max_length=200)
-    
+
+class CustomUser(User): #Herencia de tabla Usuarios, cambiado a autores
     class Meta:
-        app_label = 'Blog'
-        db_table = 'p_author'
+        proxy = True
+        app_label = 'auth'
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
-    def __str__(self):
-        return '{}'.format(self.alias)
-
+RATE_TYPE = (
+    ('1','Aburrido'),
+    ('2','Irrelevante'),
+    ('3','Relevante'),
+    ('4','Entretenido'),
+    ('5','Interesante')
+)
+        
 @python_2_unicode_compatible
 class p_post(models.Model):
-    post_code = models.CharField(max_length=45,null=True)
+    post_code = models.CharField(max_length=45,null=True,blank=True)
     name = models.CharField(max_length=200)
-    sub_name = models.CharField(max_length=100,null=True)
+    sub_name = models.CharField(max_length=100,null=True,blank=True)
     post = models.TextField()
-    rate = models.IntegerField(null=True)
-    author = models.ForeignKey(p_author)
-    cover_image = models.CharField(max_length=100,null=True)
-    date_post = models.CharField(max_length=45,null=True)
+    rate = models.CharField(max_length=1,choices=RATE_TYPE,null=True,blank=True)
+    rate_count = models.IntegerField(blank=True,null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    cover_image = models.CharField(max_length=100,null=True,blank=True)
+    date_post = models.CharField(max_length=45,null=True,blank=True)
     schedule_post = models.CharField(max_length=45)
-    posted = models.IntegerField()
+    posted = models.IntegerField(blank=True)
     
     class Meta:
         app_label = 'Blog'
         db_table = 'p_post'
+        verbose_name = 'Post'
+        verbose_name_plural = "Posts"
 
     def __str__(self):
         return '{}'.format(self.name)
